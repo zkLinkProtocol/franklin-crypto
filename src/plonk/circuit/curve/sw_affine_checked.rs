306506416489c9bbb05e26bae2c1882a5a376963
Konstantce <kopanarin@yandex.ru>
2023-01-17 18:42:07 +0400

    curve arithmetic: checking the idea for new coordinates2

--- src/plonk/circuit/curve/sw_affine_checked.rs
+++ src/plonk/circuit/curve/sw_affine_checked.rs
@@ -21,6 +21,8 @@ use crate::bellman::{
 use plonk::circuit::bigint::*;
 
 
+// NB: works only if there are no points of order 2 on the curve
+// So it al least works for all our three curves of interest
 #[derive(Clone, Debug)]
 pub struct AffinePointChecked<'a, E: Engine, G: GenericCurveAffine, T: Extension2Params<G::Base>>  
 where <G as GenericCurveAffine>::Base: PrimeField 
@@ -117,18 +119,16 @@ where <G as GenericCurveAffine>::Base: PrimeField, G: rand::Rand {
         chain.add_neg_term(&self.inner.x).add_neg_term(&other.x);
         let new_x = lambda.square_with_chain(cs, chain)?;
 
-        // lambda * -(x - new_x) + (- y)
-        let new_x_minus_this_x = new_x.sub(cs, &self.inner.x)?;
+        // lambda * (x - new_x) + (- y)
+        let this_x_minus_new_x = self.inner.x.sub(cs, &new_x)?;
         let mut chain = FieldElementsChain::new();
         chain.add_neg_term(&self.inner.y);
-        let new_y = FieldElement::mul_with_chain(cs, &lambda, &new_x_minus_this_x, chain)?;
+        let new_y = FieldElement::mul_with_chain(cs, &lambda, &this_x_minus_new_x, chain)?;
 
         let selected_x = FieldElement::conditionally_select(cs, &self.is_infty, &other.x, &new_x)?;
         let selected_y = FieldElement::conditionally_select(cs, &self.is_infty, &other.y, &new_y)?;  
 
-        println!("before error");
         let inner = unsafe { AffinePoint::from_xy_unchecked(selected_x, selected_y, self.inner.circuit_params) };
-        println!("after error");
         let is_infty = Boolean::and(cs, &coords_are_equal, &self.is_infty.not())?;
         Ok(AffinePointChecked { inner, is_infty})
     }
@@ -187,5 +187,74 @@ where <G as GenericCurveAffine>::Base: PrimeField, G: rand::Rand {
 
         let AffinePointChecked { inner, is_infty } = acc;
         Ok((inner, is_infty))
+    }
+
+    #[track_caller]
+    pub fn add<CS>(&mut self, cs: &mut CS, other: &mut Self) -> Result<Self, SynthesisError> 
+    where CS: ConstraintSystem<E>
+    {
+        let xs_are_equal = FieldElement::equals(cs, &mut self.inner.x, &mut other.inner.x)?;
+        let mut other_negated_y = other.inner.y.negate(cs)?;
+        let ys_are_equal = FieldElement::equals(cs, &mut self.inner.y, &mut other_negated_y)?;
+        let coords_are_equal = Boolean::and(cs, &xs_are_equal, &ys_are_equal)?;
+
+        let lambda_for_double = {
+            // lambda = (3 x^2) / (2 * y) 
+            let x_squared = self.inner.x.square(cs)?;
+            let x_squared_mul_3 = x_squared.scale(cs, 3)?;
+            let two_y = self.inner.y.double(cs)?;
+            FieldElement::div(&x_squared_mul_3, cs, &two_y)?
+        };
+        let lambda_for_add = {
+            let mut chain = FieldElementsChain::new();
+            let x_eq_flag_as_fe = FieldElement::from_boolean(
+                &xs_are_equal, &other.inner.circuit_params.base_field_rns_params
+            );
+            chain.add_pos_term(&other.inner.x).add_neg_term(&self.inner.x).add_pos_term(&x_eq_flag_as_fe);
+            let denom = FieldElement::collapse_chain(cs, chain)?;
+            let mut chain = FieldElementsChain::new();
+            chain.add_pos_term(&other.inner.y).add_neg_term(&self.inner.y);
+            FieldElement::div_with_chain(cs, chain, &denom)?
+        };
+        let lambda = FieldElement::conditionally_select(cs, &xs_are_equal, &lambda_for_double, &lambda_for_add)?;
+
+        let mut chain = FieldElementsChain::new();
+        chain.add_neg_term(&self.inner.x).add_neg_term(&other.inner.x);
+        let new_x = lambda.square_with_chain(cs, chain)?;
+
+        // lambda * (x - new_x) + (- y)
+        let this_x_minus_new_x = self.inner.x.sub(cs, &new_x)?;
+        let mut chain = FieldElementsChain::new();
+        chain.add_neg_term(&self.inner.y);
+        let new_y = FieldElement::mul_with_chain(cs, &lambda, &this_x_minus_new_x, chain)?;
+
+        // resulting_inner is equal to:
+        // [self.is_infty] -> other.inner, other.is_infty -> self.inner, and (new_x, new_y) otherwise
+        let mut selected_x = FieldElement::conditionally_select(cs, &self.is_infty, &other.inner.x, &new_x)?;
+        selected_x = FieldElement::conditionally_select(cs, &other.is_infty, &self.inner.x, &selected_x)?;
+        let mut selected_y = FieldElement::conditionally_select(cs, &self.is_infty, &other.inner.y, &new_y)?;
+        selected_y = FieldElement::conditionally_select(cs, &other.is_infty, &self.inner.y, &selected_y)?;
+        let inner = unsafe { AffinePoint::from_xy_unchecked(selected_x, selected_y, self.inner.circuit_params) };
+        
+        // is_infty = either booth_are_infty or both are not infty and coordinates are equal
+        let both_is_infty = Boolean::and(cs, &self.is_infty, &other.is_infty)?;
+        let tmp_check = Boolean::and(cs, &both_is_infty.not(), &coords_are_equal)?;
+        let is_infty = Boolean::or(cs, &both_is_infty, &tmp_check)?;
+
+        Ok(AffinePointChecked { inner, is_infty})
+    }
+
+    pub fn double_and_add<CS: ConstraintSystem<E>>(
+        &mut self, cs: &mut CS, other: &mut Self
+    ) -> Result<Self, SynthesisError> {
+        let mut tmp = Self::double(&self, cs)?;
+        tmp.add(cs, other)
+    }  
+
+    pub fn sub<CS: ConstraintSystem<E>>(
+        &mut self, cs: &mut CS, other: &mut Self
+    ) -> Result<Self, SynthesisError> {
+        let mut tmp = Self::negate(other, cs)?;
+        self.add(cs, &mut tmp)
     }
 }
